cb300e2c4a8741c9ade19b7806bd7f6779a50d7d
Amir Husain <amir.developer786@outlook.com>
2026-03-21 12:12:34 +0530

    Agent skills upgraded with summary screen

---
+++ components/screens/SummaryScreen.tsx
@@ -0,0 +1,13 @@
+import { ScrollView } from "react-native";
+import { Gradient } from "../gradient";
+
+export default function SessionScreen() {
+    return (
+        <>
+            <Gradient position="bottom" isSpeaking={false} />
+            <ScrollView>
+
+            </ScrollView>
+        </>
+    )
+}
